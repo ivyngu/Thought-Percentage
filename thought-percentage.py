2edea0936ae37c5a3a5c104a88e39191aae14f68
Ivy Nguyen <ivyng@seas.upenn.edu>
2022-05-14 20:47:07 -0400

    Add files via upload

--- thought-percentage.py
+++ thought-percentage.py
@@ -1,45 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import TextBox
-from matplotlib.widgets import Button
+from matplotlib.widgets import TextBox, Button
 
-x = []
-labels = []
-fig, ax = plt.subplots(figsize=(6, 6))
-ax.pie(x, labels=labels, autopct='%.1f%%')
+def draw_pie():
+    pie_ax.clear()
 
-def create_pie():
-    ax.clear()
-    patches, texts, pcts = ax.pie(
-    x, labels=labels, autopct='%.1f%%',
-    wedgeprops={'linewidth': 3.0, 'edgecolor': 'white'},
-    textprops={'size': 'x-large'})
+    patches, texts, pcts = pie_ax.pie(
+        thoughts.values(), labels=thoughts.keys(), autopct='%.1f%%',
+        wedgeprops={'linewidth': 3.0, 'edgecolor': 'white'},
+        textprops={'size': 'x-large'})
+
     plt.setp(pcts, color='white', fontweight='bold')
-    ax.set_title('My Thoughts', fontsize=18)
-    plt.tight_layout()
-def update_text(expression):
-    global ex
-    ex = expression
-    ax.relim()
-    ax.autoscale_view()
-    plt.draw()
-class Index:
-    def next(self, event):
-        if ex not in labels:
-            labels.append(ex)
-            x.append(1)
-        else:
-            index = labels.index(ex)
-            x[index] = x[index] + 1
-        create_pie()
-        plt.draw()
+    pie_ax.set_title('My Thoughts', fontsize=18)
 
-axbox = fig.add_axes([0.1, 0.05, 0.8, 0.075])
-text_box = TextBox(axbox, "Add Thought", textalignment="center")
-text_box.on_text_change(update_text)
+    fig.canvas.draw_idle()
 
-nextThought = Index()
-axnext = plt.axes([0.8, 0.05, 0.1, 0.075])
-bnext = Button(axnext, 'Add')
-bnext.on_clicked(nextThought.next)
+def on_textbox_update(inp):
+    global text_input
+    text_input = inp
+    fig.canvas.draw_idle()
+
+def next_thought(_):
+    if text_input not in thoughts:
+        thoughts[text_input] = 0
+    thoughts[text_input] += 1
+    draw_pie()
+
+def clear_chart(_):
+    global thoughts
+    pie_ax.clear()
+    thoughts = {}
+    draw_pie()
+
+# Initial set up of pie chart
+thoughts = {}
+fig, pie_ax = plt.subplots(figsize=(7, 7))
+draw_pie()
+
+# Textbox
+axbox = fig.add_axes([0.1, 0.005, 0.4, 0.075])
+text_box = TextBox(axbox, "", textalignment="center")
+text_box.on_text_change(on_textbox_update)
+
+# Button for adding thought
+axnext = fig.add_axes([0.5, 0.005, 0.2, 0.075])
+bnnext = Button(axnext, 'Add Thought')
+bnnext.on_clicked(next_thought)
+
+# Button for clearing grid
+axclear = fig.add_axes([0.7, 0.005, 0.2, 0.075])
+bnclear = Button(axclear, 'Clear Chart')
+bnclear.on_clicked(clear_chart)
+
 plt.show()
